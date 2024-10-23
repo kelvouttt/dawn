a32ca67f1f6bf8607a2b15018b4f7cab29fe5343
Kelvin Gunawan <kelvin0898@live.com>
2024-10-24 09:22:03 +1100

    Adding bit of style and Stack functions to ensure user is logged in

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,28 @@
 import Link from 'next/link';
+import { stackServerApp } from "@/stack";
+import SideBar from "@/components/SideBar";
 
 export default async function Home() {
 
-  const data = await fetch('http://localhost:8888/sections', {cache: 'no-store'});
+  const data = await fetch('http://localhost:8888/sections');
   const sections = await data.json();
-  
+
+  await stackServerApp.getUser({ or: 'redirect' });
+
   return (
-    <div className="m-8 p-8 prose">
-      <h1 className="pb-5">Learn</h1>
-      <ul>
-        {sections.map((section) => 
-          <li key={ section.section_id }>
-            <Link href={`/learn/${section.slug_field}`} className='hover:text-indigo-800 hover:text-lg'>{section.section_name}</Link>
-          </li>
-        )}
-      </ul>
+    <div className="flex flex-col max-w-none min-h-screen m-8 md:my-24 md:mx-24 lg:my-12 lg:mx-24 2xl:my-16 2xl:mx-48 prose">
+      <div className="mb-6">
+        <h1>Start learning</h1>
+      </div>
+      <div className="">
+        <ul>
+          {sections.map((section) => 
+            <li key={ section.section_id }>
+              <Link href={`/learn/${section.slug_field}`} className='hover:text-indigo-800 hover:text-lg'>{section.section_name}</Link>
+            </li>
+          )}
+        </ul>
+      </div>
     </div>
-  );
+  )
 }
