661b89ad18a4442fce6949c827750a9008f3b445
Kelvin Gunawan <kelvin0898@live.com>
2024-11-14 21:06:37 +1100

    Refactor a bit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,23 @@
 import { Elysia } from "elysia";
 import { db } from "./database"
 
-const app = new Elysia().listen(3001)
-
-console.log(
-  `🦊 Elysia is running on port ${app.server?.port}`
-);
-
-app.get('/users', async () => {
-  const rows = await db.execute('SELECT * FROM USERS');
-  return rows;
-})
+// app.listen needs to be standalone like below, otherwise the query won't work!
+const app = new Elysia()
 
 app.get('/sections', async () => {
-  const rows = await db.execute('SELECT * FROM SECTIONS');
-  return rows;
-})
+  try {
+    const rows = await db.execute('SELECT * FROM SECTIONS');
+    return {
+      data: rows
+    }} catch (error) {
+      console.error('Database error:', error);
+      return {
+        success: false,
+        error: 'Failed to fetch SECTIONS table'
+      };
+    }
+  }
+)
 
 app.get('/sections/:slug', async ({ params }) => {
   const { slug } = params;
@@ -25,7 +27,6 @@ app.get('/sections/:slug', async ({ params }) => {
   }
 )
 
-app.get('/users', async () => {
-  const rows = await db.execute('SELECT * FROM USERS');
-  return rows;
-})
+app.listen(8888, () => {
+  console.log(`🦊 Elysia is running on ${app.server?.hostname}:${app.server?.port}`)
+});
