61b13720e5fd48435ae269d8b55b890a589c9f14
Kelvin Gunawan <kelvin0898@live.com>
2024-11-19 22:14:26 +1100

    Changing the env variables used and change test connection query

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -3,14 +3,17 @@ const mariadb = require('mariadb');
 export const db = await mariadb.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    // password: process.env.DB_PASSWORD,
+    password: process.env.DB_ROOT_PASSWORD,
     database: process.env.DB_NAME,
-    connectionLimit: 5
+    port: Number(process.env.DB_PORT),
+    connectionLimit: 20000
 });
 
 async function testConnection() {
       try {
-        const [rows] = await db.execute('SELECT * FROM SECTIONS');
+        const [rows] = await db.execute('SELECT 1');
+        console.log(rows)
         console.log('💾 Database connected successfully');
      } catch (error) {
         console.log('Database connection failed', error)
