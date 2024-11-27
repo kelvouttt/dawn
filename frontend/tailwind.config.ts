21f5aa261f78bde61c8e188a48783d2aaf1f1f47
Kelvin Gunawan <kelvin0898@live.com>
2024-11-27 20:32:39 +1100

    Add .html file in tailwind config so styling can be picked up

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/*.html",
   ],
   theme: {
     extend: {
