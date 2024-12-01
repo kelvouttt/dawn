4f2cfbd76823c38a431923d51770cdc6a4551dda
Kelvin Gunawan <kelvin0898@live.com>
2024-12-02 10:02:55 +1100

    Adding auxiliary verb page

---
+++ frontend/src/app/auxiliary-verb/page.tsx
@@ -0,0 +1,7 @@
+export default async function AuxiliaryVerb() {
+    return (
+        <div className="flex flex-col max-w-none min-h-screen m-8 md:my-24 md:mx-24 lg:my-12 lg:mx-24 2xl:my-16 2xl:mx-48">
+            <p>Hello World!</p>
+        </div>
+    )
+}
